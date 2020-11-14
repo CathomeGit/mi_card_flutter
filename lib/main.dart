cae30ce964e3f442e99fc1ca99bed6789ba294e7
CathomeGit <cathome.daily.music@gmail.com>
2020-11-14 21:12:33 +0300

    50. Text boxes

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 backgroundImage: AssetImage('images/avatar.png'),
@@ -34,6 +33,38 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     color: Colors.teal[100],
                     fontFamily: 'SourceSansPro'),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(Icons.phone, color: Colors.teal),
+                    SizedBox(width: 10.0),
+                    Text('+7 916 000-00-00',
+                        style: TextStyle(
+                            color: Colors.teal[900],
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20.0))
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(width: 10.0),
+                    Text('jane_doe@mail.com',
+                        style: TextStyle(
+                            color: Colors.teal[900],
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20.0))
+                  ],
+                ),
               )
             ],
           ),
