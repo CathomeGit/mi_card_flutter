a60d8299c65634f91e8bb7f22b3bc578cee9ef21
CathomeGit <cathome.daily.music@gmail.com>
2020-11-14 20:40:28 +0300

    48-49. Add avatar + custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/avatar.png'),
+                radius: 50.0,
+              ),
+              Text(
+                'Tatiana Danilina',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'JAVA DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 5.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal[100],
+                    fontFamily: 'SourceSansPro'),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
