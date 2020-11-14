587e7372a1b1c7e5440db376ad218d09c7b36cec
CathomeGit <cathome.daily.music@gmail.com>
2020-11-14 19:07:46 +0300

    Revert "47. Layout challenge"
    This reverts commit 95ae841c

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.blue,
-                width: 100,
-              ),
-            ],
-          ),
-        ),
+        body: Container(),
       ),
-    );
-  }
+    ),
+  );
 }
